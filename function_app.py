173cd9d218f63317e93b744fc09fdc53f552db78
Raul Jarquin <raul@xix6s.com>
2026-04-06 19:34:32 -0400

    updates

--- function_app.py
+++ function_app.py
@@ -5,8 +5,13 @@ import argparse
 import os
 from pathlib import Path
 from typing import List, Dict, Any, Iterable, Tuple
+import json
 
 from dotenv import load_dotenv
+from rich.console import Console
+from rich.table import Table as RichTable
+from rich import box
+
 from azure.ai.documentintelligence import DocumentIntelligenceClient
 from azure.ai.documentintelligence.models import AnalyzeDocumentRequest
 from azure.core.credentials import AzureKeyCredential
@@ -48,39 +53,62 @@ def idocumentshttp(req: func.HttpRequest) -> func.HttpResponse:
         def analyze_file(client: DocumentIntelligenceClient, file_path: str) -> Dict[str, Any]:
             with open(file_path, "rb") as f:
                 document = f.read()
-            poller = client.begin_analyze_document(MODEL_ID,body=AnalyzeDocumentRequest(bytes_source=f.read()))
+            poller = client.begin_analyze_document(MODEL_ID, body=AnalyzeDocumentRequest(bytes_source=document))
             result = poller.result()
             return result
         
         def extract_tables(result: Dict[str, Any]) -> List[Dict[str, Any]]:
-            tables_out = List[Dict[str, Any]] = []
+            tables_out: List[Dict[str, Any]] = []
             for idx, table in enumerate(result.tables or []):
                 rows = table.row_count
                 columns = table.column_count
                 #initialize matrix
                 matrix: List[List[str]] = [["" for _ in range(columns)] for _ in range(rows)]
                 for cell in table.cells:
-                    r = cell.row_index
-                    c = cell.column_index
+                    row_index = cell.row_index
+                    column_index = cell.column_index
                     text = cell.content if hasattr(cell, "content") else ""
                     #handle Spans
                     row_span = getattr(cell, "row_span", 1) or 1
                     column_span = getattr(cell, "column_span", 1) or 1
-                    for r in range(r, r + row_span):
-                        for j in range(c, c + column_span):
-                            matrix[i][j] = text
-                    tables_out.append({
-                        "table_index": idx,
-                        "row_count": rows,
-                        "column_count": columns,
-                        "cells": matrix
-                    })
-                   
+                    for r in range(row_index, row_index + row_span):
+                        for c in range(column_index, column_index + column_span):
+                            matrix[r][c] = text
+                tables_out.append({
+                    "table_index": idx,
+                    "row_count": rows,
+                    "column_count": columns,
+                    "cells": matrix
+                })
             return tables_out
+        
+        def save_as_csv(tables: List[Dict[str, Any]], output_dir: Path, stem: str):
+            for t in tables:
+                filename = output_dir / f"{stem}_table.csv"
+                with open(filename, "w", newline="", encoding="utf-8") as f:
+                    writer = csv.writer(f)
+                    for row in t["cells"]:
+                        writer.writerow(row)
+        def save_as_json(tables: List[Dict[str, Any]], output_dir: Path, stem: str):
+            filename = output_dir / f"{stem}_table.json"
+            with open(filename, "w", encoding="utf-8") as f:
+                    json.dump(tables, f, ensure_ascii=False, indent=4)
+                    
+                    
+        def pretty_print_tables(tables: List[Dict[str, Any]]):
+            for t in tables:
+                table = RichTable(title=f"Table {t['table_index']}", box=box.SIMPLE) #fix
+                #add columns
+                for c in range(t["column_count"]):
+                    table.add_column(f"Column {c}")
+                #add rows
+                for r in range(t["row_count"]):
+                    row_data = t["cells"][r]
+                    table.add_row(*row_data)
+
+                
+
         
         #return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.", status_code=200)
